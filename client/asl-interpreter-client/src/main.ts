882abbc25bf26e42ea806b3b56c68cfd888e67e9
strawhatjay <cabrerajason7@gmail.com>
2024-07-16 22:51:11 -0700

    added tailwind css

--- client/asl-interpreter-client/src/main.ts
+++ client/asl-interpreter-client/src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import './index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -13,15 +14,15 @@ app.use(router)
 
 // add a guard to prevent users from seeing protected pages
 // only make the login page accessible for not logged in users (for now)
-// router.beforeEach((to, from ) => {
-//     // first check if the user is logged in somehow
+router.beforeEach((to, from ) => {
+    // first check if the user is logged in somehow
 
-//     // let the user go to the login page only if they're not signed in already 
-//     // if they're signed in, just send them back home!
-//     if (from.name !== 'login' && to.name == 'login') 
-//         return true; 
-//     else 
-//         return { name: 'login' };
-// })
+    // let the user go to the login page only if they're not signed in already 
+    // if they're signed in, just send them back home!
+    if (from.name !== 'login' && to.name == 'login') 
+        return true; 
+    else 
+        return { name: 'login' };
+})
 
 app.mount('#app')
---
+++ client/asl-interpreter-client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // purge should only be used in production builds but we'll leave it here 
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'] 
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
